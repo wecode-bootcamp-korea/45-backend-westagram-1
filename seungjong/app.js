0d57bb5112d6c6ae6da79fc838127f902a542572
DoorWinBell <msj102525@gmail.com>
2023-04-18 12:03:27 +0900

    add memo

--- seungjong/app.js
+++ seungjong/app.js
@@ -12,24 +12,24 @@ app.use(logger('dev'));
 app.use(express.json());
 
 
-// const dataSource = new DataSource({
-//     type: process.env.DB_CONNECTION,
-//     host: process.env.DB_HOST,
-//     port: process.env.DB_PORT,
-//     username: process.env.DB_USERNAME,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_DATABASE,
-//     logging : process.env.DB_LOGGING === "TRUE",
-// })
+const dataSource = new DataSource({
+    type: process.env.DB_CONNECTION,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    logging : process.env.DB_LOGGING === "TRUE",
+})
 
-// dataSource.initialize()
-// .then(() => {
-//     console.log("Data Source has been initialized!");
-// })
+dataSource.initialize() // db와 연결
+.then(() => {
+    console.log("Data Source has been initialized!");
+})
 
-// app.get('/', (req, res, next) => {
-//     res.status(200).json({message: "Hello World!!"});
-// })
+app.get('/', (req, res, next) => {
+    res.status(200).json({message: "Hello World!!"});
+})
 
 // /ping으로 들가면 pong함
 app.get('/ping', (req, res, next) => {
