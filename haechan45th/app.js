21566ca281b101b01d24a9129dcf10c5def31d35
hecyoon <84567556+hecyoon@users.noreply.github.com>
2023-04-18 11:05:03 +0900

    ADD app.post API to retrieve data from SQL

--- haechan45th/app.js
+++ haechan45th/app.js
@@ -1,39 +1,39 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const logger = require('morgan');
-const express = require('express');
-const cors = require('cors');
-const { DataSource } = require('typeorm');
+const logger = require("morgan");
+const express = require("express");
+const cors = require("cors");
+const { DataSource } = require("typeorm");
 const port = 8000;
 
 const app = express();
 
 const dataSource = new DataSource({
-    type: process.env.DB_CONNECTION,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
-})
-
-dataSource.initialize()
-    .then(() => {
-        console.log("Data Source has been initialized!")
-})
-
-app.use(cors());
-app.use(logger('dev'));
-app.use(express.json());
-
-app.get('/ping', function(req, res, next){
-    res.status(200).json({message: 'pong'})
+  type: process.env.DB_CONNECTION,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
 
-app.post('/users/signup', async function (req, res, next){
-    const {email, password, name, age, phoneNumber} = req.body
+dataSource.initialize().then(() => {
+  console.log("Data Source has been initialized!");
+});
 
-    await dataSource.query(`INSERT INTO users(
+app.use(cors());
+app.use(logger("dev"));
+app.use(express.json());
+
+app.get("/ping", function (req, res, next) {
+  res.status(200).json({ message: "pong" });
+});
+
+app.post("/users/signup", async function (req, res, next) {
+  const { email, password, name, age, phoneNumber } = req.body;
+
+  await dataSource.query(
+    `INSERT INTO users(
         email,
         password,
         name,
@@ -46,42 +46,66 @@ app.post('/users/signup', async function (req, res, next){
             ?,
             ?
         )
-    `,[email, password, name, age, phoneNumber])
+    `,
+    [email, password, name, age, phoneNumber]
+  );
 
-    res.status(201).json({message:"userCreated"})
+  res.status(201).json({ message: "userCreated" });
 });
 
-app.post('/users/posts', async function (req, res, next) {
-    const { userEmail, title, content, imageUrl } = req.body;
+app.post("/users/posts", async function (req, res, next) {
+  const { userEmail, title, content, imageUrl } = req.body;
 
-    const [user] = await dataSource.query(`
+  const [user] = await dataSource.query(
+    `
         SELECT id FROM users WHERE email = ?
-    `, [userEmail]);
+    `,
+    [userEmail]
+  );
 
-    if (user) {
-        const user_id = user.id;
+  if (user) {
+    const user_id = user.id;
 
-        await dataSource.query(`INSERT INTO posts(
+    await dataSource.query(
+      `INSERT INTO posts(
             user_id,
             title,
             content,
             image_url
             ) VALUES (
-                ?,
-                ?,
-                ?,
-                ?
+              ?,
+              ?,
+              ?,
+              ?
             )
-        `, [user_id, title, content, imageUrl]);
+        `,
+      [user_id, title, content, imageUrl]
+    );
 
-        res.status(201).json({ message: "🎉 post has Created!!! 🎉 " });
-    } else {
-        console.log('User not found');
-        res.status(404).json({ message: 'User not found' });
-    }
+    res.status(201).json({ message: "🎉 post has Created!!! 🎉 " });
+  } else {
+    console.log("User not found");
+    res.status(404).json({ message: "User not found" });
+  }
 });
 
+app.get('/posts', async(req, res) => {
+  await dataSource.query(
+    `SELECT 
+      posts.id,
+      users.name AS Author,
+      posts.title,
+      posts.content,
+      posts.image_url,
+      posts.created_at
+      FROM posts 
+      INNER JOIN users ON users.id = posts.user_id
+    `, (err, rows) => {
+      res.status(200).json(rows);
+    }
+  );
+});
 
-app.listen(port, function(){
-    console.log(`server listening on port ${port}`)
+app.listen(port, function () {
+  console.log(`server listening on port ${port}`);
 });
