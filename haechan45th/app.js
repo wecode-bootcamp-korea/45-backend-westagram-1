bfa3a5d0c38daf8ac655ca65599018f3787b36e6
hecyoon <84567556+hecyoon@users.noreply.github.com>
2023-04-18 15:46:23 +0900

    ASSIGNMENT 7

--- haechan45th/app.js
+++ haechan45th/app.js
@@ -160,6 +160,24 @@ app.put('/posts/:userId', async (req, res) => {
   }
 });
 
+// Deleting User's Posts API
+app.delete('/posts/:postsId', async (req, res) =>{
+  try {
+    const { postsId } = req.params;
+
+    const deletePosts = await dataSource.query(
+      `DELETE FROM posts
+      WHERE posts.id = ${postsId}
+      `
+    )
+
+    res.status(200).json({ message: 'Post DELETED successfully.' });
+  } catch(error) {
+    console.log(error);
+    res.status(400).json({message: "Error has occur in DELETING USER POSTS"})
+  }
+});
+
 app.listen(port, function () {
   console.log(`server listening on port ${port}`);
 });
