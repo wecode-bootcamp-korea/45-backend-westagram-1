33eb38575b2f13e7585ca4cb3f07c92edd387b76
hecyoon <84567556+hecyoon@users.noreply.github.com>
2023-04-18 17:15:06 +0900

    ADDED Like API/ User can like posts that is exists and it will added to the like tables

--- haechan45th/app.js
+++ haechan45th/app.js
@@ -178,6 +178,30 @@ app.delete('/posts/:postsId', async (req, res) =>{
   }
 });
 
+// Likes API
+app.post('/users/likes', async(req, res) => {
+  try { 
+    const { user_id, postId } = req.body;
+
+    const likePosts = await dataSource.query(
+      `INSERT INTO likes(
+        user_id,
+        post_id
+        ) VALUES (
+          ?,
+          ?
+        )
+    `,
+  [user_id, postId]
+    )
+    
+    res.status(200).json({ message: 'Post LIKED ❤️ successfully.' });
+  } catch(error) {
+    console.log(error);
+    res.status(400).json({message: "Error has occur in LIKE USER POSTS"})
+  }
+});
+
 app.listen(port, function () {
   console.log(`server listening on port ${port}`);
 });
