44f4a4547ca8c966a202b883b4e43e519be744b1
DoorWinBell <msj102525@gmail.com>
2023-04-19 14:12:01 +0900

    branch 수정

--- seungjong/app.js
+++ seungjong/app.js
@@ -1,46 +1,67 @@
 require('dotenv').config();
-
-const cors = require('cors');
-const logger = require('morgan');
+const cors = require("cors");
+const logger = require("morgan");
 const express = require('express');
-const { DataSource } = require('typeorm');
-
+const { DataSource } = require("typeorm");
 const app = express();
+const port = 4000;
 
 app.use(cors());
-app.use(logger('dev'));
+app.use(logger("combined"));
 app.use(express.json());
 
+const dataSource = new DataSource({
+    type: process.env.DB_CONNECTION,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    logging: process.env.DB_LOGGING
+})
 
-// const dataSource = new DataSource({
-//     type: process.env.DB_CONNECTION,
-//     host: process.env.DB_HOST,
-//     port: process.env.DB_PORT,
-//     username: process.env.DB_USERNAME,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_DATABASE,
-//     logging : process.env.DB_LOGGING === "TRUE",
-// })
+dataSource.initialize()
+    .then(() => {
+        console.log("Data Source has benn initializd!")
+    })
+    .catch(() => {
+        console.log("Fail!!")
+    })
 
-// dataSource.initialize()
-// .then(() => {
-//     console.log("Data Source has been initialized!");
-// })
 
-// app.get('/', (req, res, next) => {
-//     res.status(200).json({message: "Hello World!!"});
-// })
-
-// /ping으로 들가면 pong함
-app.get('/ping', (req, res, next) => {
-    console.log('aaaa')
-    res.status(200).json({message: "pong!!"});
+app.get('/', (req, res, next) => {
+    res.status(200).json({message : "Hello!"});
 });
 
+// health check
+app.get('/ping', (req, res, next) => {
+    res.status(200).json({ message: "pong" });
+})
 
-const port = process.env.PORT;
+// signUp
+
+app.post('/signUp', async (req, res, next) => {
+    const {name, email, password, profile_image } = req.body;
+    console.log(req);
+
+    await dataSource.query(
+        `INSERT INTO users(
+            name,
+            email,
+            password,
+            profile_image
+        ) VALUES (?, ?, ?, ?);
+        `,
+        [name, email, password, profile_image]
+    );
+    res.status(201).json({ message: "sucessfully created!" });
+
+})
+
+
+
+
 
 app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
+    console.log(`Sever listiening on ${port}`);
 });
-
