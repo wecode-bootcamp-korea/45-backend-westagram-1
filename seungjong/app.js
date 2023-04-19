f90c2e936be291cc0c0562b9d04a7c3e368b1f8d
DoorWinBell <msj102525@gmail.com>
2023-04-19 10:58:02 +0900

    .env.sample 수정, app.js logging : process.env.DB_LOGGING부분 수정

--- seungjong/app.js
+++ seungjong/app.js
@@ -19,7 +19,7 @@ const dataSource = new DataSource({
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    logging : process.env.DB_LOGGING === "TRUE",
+    logging : process.env.DB_LOGGING,
 })
 
 dataSource.initialize() 
