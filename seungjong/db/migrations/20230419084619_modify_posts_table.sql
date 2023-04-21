5b4d6d6e4e17da4fb59ce8b0ae01604ecb4d3140
DoorWinBell <msj102525@gmail.com>
2023-04-21 22:41:21 +0900

    signup LP

--- seungjong/db/migrations/20230419084619_modify_posts_table.sql
+++
@@ -1,5 +0,0 @@
--- migrate:up
-ALTER TABLE posts ADD image_url VARCHAR(3000) Null;
-
--- migrate:down
-
---
+++ seungjong/models/userDao.js
@@ -0,0 +1,45 @@
+// models/userDao.js
+
+const { DataSource } = require('typeorm');
+
+const dataSource = new DataSource({
+    type: process.env.DB_CONNECTION,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    logging: process.env.DB_LOGGING
+})
+
+dataSource.initialize()
+.then(() => {
+    console.log("Data Soure has been initialized!");
+})
+.catch((err) => {
+    console.log("Error occurred durung Data Source initialization!", err);
+        dataSource.destroy();
+});
+
+const createUser = async ( name, email, password, profileImage ) => {
+	try {
+		return await dataSource.query(
+		`INSERT INTO users(
+		    name,
+		    email,
+		    password,
+		    profile_image
+		) VALUES (?, ?, ?, ?);
+		`,
+		[ name, email, password, profileImage ]
+	  );
+	} catch (err) {
+		const error = new Error('INVALID_DATA_INPUT');
+		error.statusCode = 500;
+		throw error;
+	}
+};
+
+module.exports = {
+    createUser
+}
