143605fae2e1fab455cf3d1afdee66aea266c1eb
DoorWinBell <msj102525@gmail.com>
2023-04-25 20:08:08 +0900

    bcyrpt complete

--- seungjong/services/userService.js
+++ seungjong/services/userService.js
@@ -1,15 +1,27 @@
 // services/userService.js
 const userDao = require("../models/userDao");
+const bcrypt = require("bcrypt");
 
-const { passwordValidationCheck } = require("../utils/validationCheck");
+const password = "password";
+const saltRounds = 12;
+
+const makeHash = async (password, saltRounds) => {
+  return await bcrypt.hash(password, saltRounds);
+};
+
+const checkHash = async (password, hashedPassword) => {
+  return await bcrypt.compare(password, hashedPassword); // (1)
+};
 
 const signUp = async (name, email, password, profileImg) => {
-  await passwordValidationCheck(password);
+  const hashedPassword = await makeHash(password, 12);
+  const result = await checkHash(password, hashedPassword);
+  console.log(`1111111111`, hashedPassword);
 
   const createUser = await userDao.createUser(
     name,
     email,
-    password,
+    hashedPassword,
     profileImg
   );
 
