66a704119ad0f52c80730a18a2c65ced72efa17d
DoorWinBell <msj102525@gmail.com>
2023-04-25 11:57:21 +0900

    like function add message

--- seungjong/controllers/likeController.js
+++ seungjong/controllers/likeController.js
@@ -4,11 +4,17 @@ const like = async (req, res) => {
     try {
         const {userId, postId} = req.params;
         console.log(`111111111:::userId::${userId},postId:::${postId}`);
-
+        
         if( !userId || !postId) {
             return res.status(400).json({message: "NOT_FOUND" });
         }
         const result = await likeService.like(userId, postId);
+        console.log(result.insertId);
+        if (result.insertId === 0) {
+            return res.status(200).json({message: "deleteLiked!"});
+        } else {
+            return res.status(200).json({message: "likeCreated!"});            
+        }
         
     } catch (err) {
         console.log(err);
