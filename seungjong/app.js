15b9b7f2c9eea2a71eb401a9cedf112559ca5aa0
DoorWinBell <msj102525@gmail.com>
2023-04-25 16:36:03 +0900

    save

--- seungjong/app.js
+++ seungjong/app.js
@@ -1,9 +1,9 @@
-const http = require('http');
-const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
+const http = require("http");
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
 
-const dotenv =require("dotenv")
+const dotenv = require("dotenv");
 dotenv.config();
 const routes = require("./routes");
 const app = express();
@@ -13,20 +13,45 @@ app.use(morgan("combined"));
 app.use(express.json());
 app.use(routes);
 
+const bcrypt = require("bcrypt");
 
-app.get('/ping', (req, res) => {
-    res.json({ message: "pong!" });
-})
+const password = "password";
+const saltRounds = 12;
+
+const makeHash = async (password, saltRounds) => {
+  return await bcrypt.hash(password, saltRounds);
+};
+
+const checkHash = async (password, hashedPassword) => {
+  return await bcrypt.compare(password, hashedPassword);
+};
+
+const main = async () => {
+  const hashedPassword = await makeHash(password, 12);
+  const result = await checkHash(password, hashedPassword);
+  console.log(result);
+};
+
+main();
+
+app.get("/ping", (req, res) => {
+  res.json({ message: "pong!" });
+});
 
 const server = http.createServer(app);
 const port = process.env.PORT;
 
 const start = async () => {
-    try {
-        server.listen(port, () => console.log(`Server is listening on ${port}`));
-    } catch (err) {
-        console.log(err);
-    }
+  try {
+    server.listen(port, () => console.log(`Server is listening on ${port}`));
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const person = {
+  name: "Door",
+  age: 1000000000,
 };
 
 start();
--- seungjong/models/likeDao.js
+++ seungjong/models/likeDao.js
@@ -1,38 +1,47 @@
-const { post } = require('../routes');
-const dataSource = require('./Dao');
+const { post } = require("../routes");
+const dataSource = require("./Dao");
 
 const like = async (userId, postId) => {
-    console.log(`3333333333:::userId::${userId},postId:::${postId}`);
-    try {
-        const [result] = await dataSource.query(`
+  console.log(`3333333333:::userId::${userId},postId:::${postId}`);
+  try {
+    const [result] = await dataSource.query(
+      `
         SELECT EXISTS (
                 SELECT id
                 FROM likes
                 WHERE user_id = ? AND post_id = ?
             ) AS isLiked;
-        `, [userId, postId]);
+        `,
+      [userId, postId]
+    );
 
-        if (!!parseInt(result.isLiked)) {
-            console.log(`삭제`);
-            return await dataSource.query(`
+    if (!!parseInt(result.isLiked)) {
+      console.log(`삭제`);
+      return await dataSource.query(
+        `
             DELETE FROM likes
             WHERE user_id = ? AND post_id = ?;
-            `, [userId, postId]);
-        } else {
-            console.log(`생성`);
-            return await dataSource.query(`
+            `,
+        [userId, postId]
+      );
+    } else {
+      console.log(`생성`);
+      return await dataSource.query(
+        `
                 INSERT INTO likes(
                     user_id,
                     post_id
                 ) VALUES (?, ?);
-            `, [userId, postId])
-        }
-    } catch (err) {
-        console.log(err);
-        const error = new Error("DATA_NOT_FOUND");
-        error.statusCode = 500;
-        throw error;
+            `,
+        [userId, postId]
+      );
     }
-}
+  } catch (err) {
+    console.log(err);
+    const error = new Error("DATA_NOT_FOUND");
+    error.statusCode = 500;
+    throw error;
+  }
+};
 
 module.exports = { like };
